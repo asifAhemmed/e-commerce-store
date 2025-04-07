404a47fc89d3a01548814150dbd0aa5c7f4a9f44
asif ahemmed <asifahemmed0@gmail.com>
2025-04-07 17:41:34 +0600

    auth-routes-created

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ import dotenv from 'dotenv';
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
 
 
 app.use("/api/auth",authRoutes);
