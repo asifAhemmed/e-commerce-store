2d1bf4feccaeb01363959d2cbeab01827130ef74
asif ahemmed <asifahemmed0@gmail.com>
2025-04-07 10:58:30 +0600

    database-connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
@@ -13,4 +14,5 @@ app.use("/api/auth",authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
