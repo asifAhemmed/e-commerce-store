ba95a69bfd23ac62ff5632ca0b1264db47325570
asif ahemmed <asifahemmed0@gmail.com>
2025-04-07 17:58:16 +0600

    cart-routes-created

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
+import cartRoutes from './routes/cart.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from "cookie-parser";
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 
 app.listen(PORT, () => {
